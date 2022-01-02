eeb37249ac6581255e1e58d24936cae56dc7de3c
Andy Chen <chenyangpoppin@hotmail.com>
2022-01-02 16:50:12 +0800

    feat: copy html pages to dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,26 @@
 const gulp = require('gulp');
 
 /*
-  -- TOP LEVEL FUNCTIONS --
-  gulp.task  - Define tasks
-  gulp.src   - Point to files to use
-  gulp.dest  - Point to folder to output
-  gulp.watch - Watch files and folders for changes
+    -- TOP LEVEL FUNCTIONS --
+    gulp.task  - Define tasks
+    gulp.src   - Point to files to use
+    gulp.dest  - Point to folder to output
+    gulp.watch - Watch files and folders for changes
 */
 
+// Logs message
+// cmd: gulp message
+gulp.task('message', () => {
+    return console.log('gulp is running...');
+});
+
+// copy all HTML pages
+gulp.task('copyHtml', () => {
+    gulp.src('src/*.html')
+        .pipe(gulp.dest('dist'));
+});
+
+// cmd: gulp
+gulp.task('default', function() {
+    return console.log('gulp is running...');
+});
