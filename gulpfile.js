8f4fd31193f35b154dbf28eb423058fcb5862bec
Andy Chen <chenyangpoppin@hotmail.com>
2022-01-02 16:34:02 +0800

    chore: install dependencies and describe gulp top functions

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+/**
+ * Describe all of our gulp tasks
+ */
+const gulp = require('gulp');
+
+/*
+  -- TOP LEVEL FUNCTIONS --
+  gulp.task  - Define tasks
+  gulp.src   - Point to files to use
+  gulp.dest  - Point to folder to output
+  gulp.watch - Watch files and folders for changes
+*/
+
