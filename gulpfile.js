16fa14113f853fda2431a922fc6865724973ebb4
Andy Chen <chenyangpoppin@hotmail.com>
2022-01-06 22:55:36 +0800

    feat: run a group of gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -16,14 +16,16 @@ const sass = require('gulp-sass')(require('sass'));
 
 // logs message
 // cmd: gulp message
-gulp.task('message', () => {
-    return console.log('gulp is running...');
+gulp.task('message', (done) => {
+    console.log('gulp is running...');
+    done();
 });
 
 // copy all HTML pages
-gulp.task('copyHtml', () => {
+gulp.task('copyHtml', (done) => {
     gulp.src('src/*.html')
         .pipe(gulp.dest('dist'));
+    done();
 });
 
 // optimize images
@@ -35,20 +37,21 @@ gulp.task('copyHtml', () => {
 
 // compress js files
 // cmd: gulp minify
-gulp.task('minify', () => {
+gulp.task('minify', (done) => {
     gulp.src('src/js/*.js')
         .pipe(uglify())
         .pipe(gulp.dest('dist/js'));
+    done();
 });
 
 // compile sass
-gulp.task('sass', () => {
+gulp.task('sass', (done) => {
     gulp.src('src/sass/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('dist/css'))
+        .pipe(gulp.dest('dist/css'));
+    done();
 });
 
 // cmd: gulp
-gulp.task('default', function() {
-    return console.log('gulp is running...');
-});
+// run a group of tasks in one task
+gulp.task('default', gulp.series('message', 'copyHtml', 'minify', 'sass'));
